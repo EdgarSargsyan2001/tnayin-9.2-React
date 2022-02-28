1483e6d2a69cb0f6c1f92f851254cec3c788f17e
EdgarSargsyan2001 <edgar.sargsyan.2001@mail.ru>
2022-02-28 21:31:55 +0400

    9.2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import Datafatch from './API/Datafatch';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [img,setimg] = useState(
+    <div>
+      <img className='img' src='https://dog.ceo/img/dog-api-logo.svg'></img>
+    </div>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Datafatch setimg={setimg} img="img" />
+      {img}
     </div>
   );
 }
